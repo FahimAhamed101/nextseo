6e1a8c73c2233baadc382e4b05100d32bba6623f
fahim <fahimahamedweb@gmail.com>
2024-09-29 00:32:20 +0600

    sitemap added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,13 +2,13 @@ import { BlogPost  } from "@/models/BlogPost";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const response = await fetch("http://127.0.0.1:8000/post/api/");
+  const response = await fetch("https://nextbackend-virid.vercel.app/post/api/");
   const data = await response.json();
   console.log(data.posts)
   const  posts  = data;
 console.log(posts)
   const postEntries: MetadataRoute.Sitemap = posts.map(({ id } :  BlogPost ) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/posts/${id}`,
+    url: `https://nextbackend-virid.vercel.app/post/api/posts/${id}`,
     // lastModified: new Date(post.updatedAt),
     // changeFrequency:,
     // priority:
